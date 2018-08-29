d3d0ede5def031d0d03c1eae36983d89994fbdfa
Evgeny Uleykin <android73.dd@gmail.com>
2018-08-30 00:34:31 +0300

    Add moxy strategies

--- app/src/main/java/ru/android73/geekstagram/ui/presentation/view/GeneralView.java
+++ app/src/main/java/ru/android73/geekstagram/ui/presentation/view/GeneralView.java
@@ -3,18 +3,26 @@ package ru.android73.geekstagram.ui.presentation.view;
 import android.support.annotation.StringRes;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 import ru.android73.geekstagram.model.ImageListItem;
 
 public interface GeneralView extends MvpView {
 
+    @StateStrategyType(AddToEndSingleStrategy.class)
     void showInfo(@StringRes int resId);
 
+    @StateStrategyType(SkipStrategy.class)
     void openCamera();
 
+    @StateStrategyType(SkipStrategy.class)
     void requestWriteExternalPermission();
 
+    @StateStrategyType(AddToEndSingleStrategy.class)
     void addItemToList(ImageListItem item);
 
+    @StateStrategyType(AddToEndSingleStrategy.class)
     void notifyDataChanged();
 }
